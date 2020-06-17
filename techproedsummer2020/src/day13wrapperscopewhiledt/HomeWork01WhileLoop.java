c83a9d871058f687fef19ff01f38a673b26abb30
ilkerakkaya <ilkerakkaya@gmail.com>
2020-06-17 17:57:14 +0200

    Changes in assginment

--- techproedsummer2020/src/day13wrapperscopewhiledt/HomeWork01WhileLoop.java
+++ techproedsummer2020/src/day13wrapperscopewhiledt/HomeWork01WhileLoop.java
@@ -6,17 +6,40 @@ public class HomeWork01WhileLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		int i=0;
 		
-		while (i<5) {
+		Scanner scan=new Scanner (System.in);
+		System.out.println("enter an integer");
+		int num=scan.nextInt();
+		
+		
+		//!st way
+		
+		int product=1;
+		for (int i=1; i<=num; i++) {
 			
-			System.out.println(i);
+			product=product*i;
+		}
+		
+		System.out.println(product);
+		
+		
+		
+		//2nd Way
+		int prdct=1;
+		
+		int i=1;
+		
+		while (i<=num) {
 			
+			prdct=prdct*i;
 			i++;
 			
 			
 		}
+		
+		System.out.println(prdct);
+		
+		
 		
 	}
 
---
+++ techproedsummer2020/src/day13wrapperscopewhiledt/HomeWork5WhileLoop.java
@@ -0,0 +1,10 @@
+package day13wrapperscopewhiledt;
+
+public class HomeWork5WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
