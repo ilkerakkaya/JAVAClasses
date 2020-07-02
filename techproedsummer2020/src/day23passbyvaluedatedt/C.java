92c8c3713b6fa478960b4c245d1ed66c045d9b07
ilkerakkaya <ilkerakkaya@gmail.com>
2020-07-02 11:46:02 +0200

    new classes

---
+++ techproedsummer2020/src/day23passbyvaluedatedt/C.java
@@ -0,0 +1,13 @@
+package day23passbyvaluedatedt;
+
+import day24varargsaccessmodifiersstringbuilderdt.A;
+
+public class C {
+
+	public static void main(String[] args) {
+		A obj3= new A();
+		
+
+	}
+
+}
